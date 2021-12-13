f31db9df9cacc5c78133619140d545495111f4a6
ptillemans <pti@snamellit.com>
2021-12-13 09:26:03 +0100

    day12 completed

--- src/main/kotlin/aoc2021/Day12.kt
+++ src/main/kotlin/aoc2021/Day12.kt
@@ -15,20 +15,22 @@ class Day12 {
     
     init {
         val filename = "/aoc2021/day12/input.txt"
-        input = Day12::class.java.getResource(filename).readText()
+        input = Day12::class.java.getResource(filename)!!.readText()
     }
     
 
     fun part1(): String =
         input.parseMaze()
-            .generatePaths()
+            .generatePaths(canVisit = ::canVisitPart1)
             .count()
             .toString()
    
-    fun part2(): String? {
-        return null
-    }
-    
+    fun part2(): String =
+        input.parseMaze()
+            .generatePaths(canVisit = ::canVisitPart2)
+            .count()
+            .toString()
+
 }
 
 fun String.parseMaze() =
@@ -43,16 +45,33 @@ fun String.parseMaze() =
 private fun Edge.otherNode(node:Node) =
     if (this.first == node) this.second else this.first
 
-fun List<Edge>.generatePaths(node: Node = START_NODE, seen: Set<Node> = setOf()): List<Path> =
+private fun Node.isLargeCave() =
+    this.all { it.isUpperCase()}
+
+fun canVisitPart1(seen: Map<Node, Int>, n: Node): Boolean =
+    n.isLargeCave() || !seen.contains(n)
+
+
+fun canVisitPart2(seen: Map<Node, Int>, n: Node): Boolean =
+    n.isLargeCave() || !seen.contains(n)
+    || seen.all { (k, v) -> k.isLargeCave() || v <= 1 }
+
+fun List<Edge>.generatePaths(
+    node: Node = START_NODE,
+    seen: Map<Node,Int> = mapOf(),
+    canVisit: Map<Node,Int>.(Node) -> Boolean = ::canVisitPart1)
+: List<Path> =
     filter { it.toList().contains(node) }
         .map { it.otherNode(node) }
-        .filter { n -> n.all { it.isUpperCase() } || !seen.contains(n) }
+        .filter { it != START_NODE }
+        .filter{ seen.canVisit(it) }
         .flatMap { n ->
             if (n == END_NODE)
                 listOf(listOf(node, n))
             else
-                this.generatePaths(n, seen + node)
+                this.generatePaths(n, seen + (n to (seen[n]?:0) + 1), canVisit)
                     .map { path -> listOf(node) + path }
+
         }
 
 
--- src/test/kotlin/aoc2021/Day12Test.kt
+++ src/test/kotlin/aoc2021/Day12Test.kt
@@ -6,13 +6,13 @@ import java.lang.System.lineSeparator
 data class Sample(
     val maze: String,
     val edges: List<Edge>,
-    val nPaths: Int,
-    val paths: List<Path>? = null
+    val nPathsPart1: Int,
+    val pathsPart1: List<Path>? = null,
+    val nPathsPart2: Int,
+    val pathsPart2: List<Path>? = null
 )
 
 class Day12Test {
-    private val input = """
-""".trimIndent()
 
     private val sample = readExample("/aoc2021/day12/sample.txt")
     private val sample2 = readExample("/aoc2021/day12/sample2.txt")
@@ -26,56 +26,103 @@ class Day12Test {
             .split(lineSeparator() + lineSeparator())
 
         val edges = parts[0].parseMaze()
-        val nPaths = parts[1].toInt()
-        if (parts.size == 2) {
-            return Sample(parts[0], edges, nPaths)
-        }
+        val nPathsPart1 = parts[1].toInt()
+        val nPathsPart2 = if (parts[2].toCharArray()[0].isDigit())
+            parts[2].toInt()
+        else
+            parts[3].toInt()
+        val pathsPart1 = if (parts[2].isPathList())
+            parts[2].parsePaths() else null
 
-        val paths = parts[2].split(lineSeparator())
-            .map { it.split(",") }
-        return Sample(parts[0], edges, nPaths, paths)
+        val pathsPart2 = if (parts.size > 3 && parts[3].isPathList())
+            parts[3].parsePaths()
+        else if (parts.size > 4 && parts[4].isPathList())
+            parts[4].parsePaths()
+        else
+            null
+
+        return Sample(parts[0], edges, nPathsPart1, pathsPart1, nPathsPart2, pathsPart2)
     }
+
+    private fun String.isPathList() = this.toCharArray()[0].isLetter()
+
+    private fun String.parsePaths() =
+        this.split(lineSeparator()).map {it.split(",")}
+
 
     private var challenge = Day12()
 
     @Test
     fun testAllPathsGenerated() {
-        val actual = sample.edges.generatePaths();
-        for (path in sample.paths!!) {
+        val actual = sample.edges.generatePaths()
+        for (path in sample.pathsPart1!!) {
             assertTrue(actual.contains(path))
         }
     }
 
     @Test
     fun `test right amount of paths generated`() {
-        val actual = sample.edges.generatePaths();
-        assertEquals(sample.nPaths, actual.size)
+        val actual = sample.edges.generatePaths()
+        assertEquals(sample.nPathsPart1, actual.size)
     }
 
     @Test
     fun `test slightly larger sample`() {
-        val actual = sample2.edges.generatePaths();
-        assertEquals(sample2.nPaths, actual.size)
+        val actual = sample2.edges.generatePaths()
+        assertEquals(sample2.nPathsPart1, actual.size)
     }
 
     @Test
     fun `test much larger sample`() {
-        val actual = sample3.edges.generatePaths();
-        assertEquals(sample3.nPaths, actual.size)
+        val actual = sample3.edges.generatePaths()
+        assertEquals(sample3.nPathsPart1, actual.size)
     }
 
     @Test
     fun testPart1() {
         challenge.input = sample3.maze
         val actual = challenge.part1()
-        val expected = sample3.nPaths.toString()
+        val expected = sample3.nPathsPart1.toString()
         assertEquals(expected, actual)
     }
-   
+
+    @Test
+    fun `test part2 with small example`() {
+        val actual = sample.edges.generatePaths(canVisit = canVisitPart2)
+        assertEquals(sample.nPathsPart2, actual.size)
+
+    }
+
+    @Test
+    fun `test part2 with larger example`() {
+        val actual = sample2.edges.generatePaths(canVisit = canVisitPart2)
+        assertEquals(sample2.nPathsPart2, actual.size)
+
+    }
+
+    @Test
+    fun `test part2 with much larger example`() {
+        val actual = sample3.edges.generatePaths(canVisit = canVisitPart2)
+        assertEquals(sample3.nPathsPart2, actual.size)
+    }
+
+    @Test
+    fun `test canVisitPart2 second visit small cave`() {
+        val seen = mapOf( "c" to 1, "A" to 2)
+        assertTrue(canVisitPart2(seen, "c"))
+    }
+
+    @Test
+    fun `test canVisitPart2 third visit small cave`() {
+        val seen = mapOf( "c" to 2, "A" to 2)
+        assertFalse(canVisitPart2(seen, "c"))
+    }
+
     @Test
     fun testPart2() {
+        challenge.input = sample3.maze
         val actual = challenge.part2()
-        val expected = null
+        val expected = sample3.nPathsPart2.toString()
         assertEquals(expected, actual)
     }
     
