3709b8ff6842b47fcbfeafcca15d0ce6500e5435
ptillemans <pti@snamellit.com>
2021-12-20 00:14:39 +0100

    completed day 19

--- src/main/kotlin/aoc2021/Day19.kt
+++ src/main/kotlin/aoc2021/Day19.kt
@@ -2,7 +2,10 @@ package aoc2021
 
 import com.google.gson.Gson
 import common.toIntList
+import java.lang.Math.abs
+import java.lang.System.`in`
 import java.lang.System.lineSeparator
+import java.util.*
 
 enum class Rotation {
     Rot0,
@@ -11,75 +14,166 @@ enum class Rotation {
     Rot270
 }
 
-data class Coordinate(val x:Int, val y:Int, val z:Int) {
+data class Coordinate(val x: Int, val y: Int, val z: Int) {
 
-    fun distance(b: Coordinate) : Int {
+    fun distance(b: Coordinate): Int {
         val dx = b.x - this.x
         val dy = b.y - this.y
         val dz = b.z - this.z
-        return dx*dx + dy*dy + dz*dz
+        return dx * dx + dy * dy + dz * dz
     }
+
+    fun manhattan(b: Coordinate): Int {
+        val dx = b.x - this.x
+        val dy = b.y - this.y
+        val dz = b.z - this.z
+        return abs(dx) + abs(dy) + abs(dz)
+    }
+
+    operator fun plus(other: Coordinate): Coordinate =
+        Coordinate(this.x + other.x, this.y + other.y, this.z + other.z)
+
+    operator fun minus(other: Coordinate): Coordinate =
+        Coordinate(this.x - other.x, this.y - other.y, this.z - other.z)
+}
+
+data class Orientation(val xAxis: Rotation, val yAxis: Rotation, val zAxis: Rotation) {
+    fun rotate(c: Coordinate): Coordinate {
+        return rotateX(rotateY(rotateZ(c)))
+    }
+
+    private fun rotateX(c: Coordinate): Coordinate =
+        when (this.xAxis) {
+            Rotation.Rot0 -> c
+            Rotation.Rot90 -> Coordinate(c.x, -c.z, c.y)
+            Rotation.Rot180 -> Coordinate(c.x, -c.y, -c.z)
+            Rotation.Rot270 -> Coordinate(c.x, c.z, -c.y)
+        }
+
+    private fun rotateY(c: Coordinate): Coordinate =
+        when (this.yAxis) {
+            Rotation.Rot0 -> c
+            Rotation.Rot90 -> Coordinate(-c.z, c.y, c.x)
+            Rotation.Rot180 -> Coordinate(-c.x, c.y, -c.z)
+            Rotation.Rot270 -> Coordinate(c.z, c.y, -c.x)
+        }
+
+    private fun rotateZ(c: Coordinate): Coordinate =
+        when (this.zAxis) {
+            Rotation.Rot0 -> c
+            Rotation.Rot90 -> Coordinate(-c.y, c.x, c.z)
+            Rotation.Rot180 -> Coordinate(-c.x, -c.y, c.z)
+            Rotation.Rot270 -> Coordinate(c.y, -c.x, c.z)
+        }
 
 }
 
-data class Orientation(val xAxis: Rotation, val yAxis: Rotation, val zAxis: Rotation)
+val allOrientations = findAllOrientations()
 
-data class Scanner(val location: Coordinate?, val orientation: Orientation?, val beacons: List<Coordinate>)
+fun findAllOrientations(): List<Orientation> {
+    val testVector = Coordinate(1, 2, 3)
+    val seen = mutableSetOf<Coordinate>()
+    val orientations = Rotation.values().flatMap { rotX ->
+        Rotation.values().flatMap { rotY ->
+            Rotation.values().map { rotZ -> Orientation(rotX, rotY, rotZ) }
+        }
+    }
+
+    var result = mutableListOf<Orientation>()
+    for (o in orientations) {
+        val vec = o.rotate(testVector)
+        if (!seen.contains(vec)) {
+            result.add(o)
+            seen.add(vec)
+        }
+    }
+
+    return result
+}
+
+data class Scanner(val name: String, var location: Coordinate?, var orientation: Orientation?, val beacons: List<Coordinate>) {
+    fun beaconLocations(): List<Coordinate> =
+        this.beacons
+            .map { c -> this.relToAbs(c) }
+
+    fun relToAbs(c: Coordinate): Coordinate =
+        this.orientation!!.rotate(c).plus(this.location!!)
+
+}
 
 class Day19 {
-    
+
     var input: String
-    
+
     init {
         val filename = "/aoc2021/day19/input.txt"
         input = Day19::class.java.getResource(filename).readText()
     }
 
-    fun part1(): String? {
-        return null
-    }
-   
-    fun part2(): String? {
-        return null
-    }
-    
+    fun part1(): String =
+        input
+            .toScanners()
+            .findAllBeacons()
+            .size
+            .toString()
+
+
+    fun part2(): String =
+        input
+            .toScanners()
+            .findLargestDifference()
+            .toString()
+
 }
 
-fun String.toScanners() : List<Scanner> =
+fun String.toScanners(): List<Scanner> =
     this.split(lineSeparator() + lineSeparator())
         .filter { isNotBlank() }
         .map { line ->
-            line.split(lineSeparator())
-                .drop(1)
-                .filter { it.isNotBlank() }
-                .map { it.toIntList() }
-                .map { Coordinate(x = it[0], y = it[1], z = it[2]) }
-        }
-        .map { Scanner(null, null, it)  }
-        .mapIndexed { i, scanner -> if (i == 0)
-            Scanner(
-                Coordinate(0,0,0),
-                Orientation(Rotation.Rot0, Rotation.Rot0, Rotation.Rot0),
-                scanner.beacons
+            Pair(
+                line.split(lineSeparator())
+                    .first()
+                    .replace("---","")
+                    .trim(),
+                line.split(lineSeparator())
+                    .drop(1)
+                    .filter { it.isNotBlank() }
+                    .map { it.toCoordinate() }
             )
-        else
-            scanner
+        }
+        .map { Scanner(it.first, null, null, it.second) }
+        .mapIndexed { i, scanner ->
+            if (i == 0)
+                Scanner(
+                    scanner.name,
+                    Coordinate(0, 0, 0),
+                    Orientation(Rotation.Rot0, Rotation.Rot0, Rotation.Rot0),
+                    scanner.beacons
+                )
+            else
+                scanner
         }
 
+fun String.toCoordinate(): Coordinate {
+    val coords = this.toIntList()
+    return Coordinate(x = coords[0], y = coords[1], z = coords[2])
+
+}
+
 data class BeaconDistance(
-    val scanner: Scanner
-    val beacons: Set<Coordinate>
+    val beacons: Set<Coordinate>,
     val distance: Int
 )
 
-fun List<Coordinate>.toDistances(scanner: Scanner): List<BeaconDistance> =
-    (0 until this.size).flatMap { i1 ->
-        (i1+1 until this.size).map { i2 ->
-            Pair(this[i1], this[i2])
+fun Scanner.toDistances(): List<BeaconDistance> =
+    (0 until this.beacons.size).flatMap { i1 ->
+        (i1 + 1 until this.beacons.size).map { i2 ->
+            BeaconDistance(
+                setOf(this.beacons[i1], this.beacons[i2]),
+                this.beacons[i1].distance(this.beacons[i2])
+            )
         }
     }
-        .map { BeaconDistance(scanner, setOf(it.first, it.second), it.first.distance(it.second)) }
-
 
 fun List<BeaconDistance>.intersection(other: List<BeaconDistance>): List<Pair<BeaconDistance, BeaconDistance>> =
     this.flatMap { beacon ->
@@ -87,10 +181,112 @@ fun List<BeaconDistance>.intersection(other: List<BeaconDistance>): List<Pair<Be
             .map { Pair(beacon, it) }
     }
 
+fun List<Pair<BeaconDistance, BeaconDistance>>.overlappingBeaconsInDistList(): Map<Coordinate, Set<Coordinate>> {
+    if (this.size == 0)
+        return mapOf()
+
+    val distMap = this.associateBy { it.first.distance }
+
+    val firstBeacons = this.map { it.first.beacons }
+        .reduce { acc, bs -> acc.union(bs) }
+
+    val secondBeacons = this.map { it.second.beacons }
+        .reduce { acc, bs -> acc.union(bs) }
+
+    val beaconMap = firstBeacons
+        .map { coord ->
+            coord to
+                    this.filter { it.first.beacons.contains(coord) }
+                        .map { bd -> distMap[bd.second.distance]!!.second.beacons }
+                        .fold(secondBeacons) { rest, bs -> rest.intersect(bs) }
+        }
+        .toMap()
+
+    return beaconMap
+}
+
+fun allowableOrientation(delta1: Coordinate, delta2: Coordinate): Set<Orientation> =
+    allOrientations
+        .filter { delta1 == it.rotate(delta2) }
+        .toSet()
+
+// takes a map of absolute coordinates of beacons to relative coords for scanner and returns
+// best estimate for location and rotation
+fun Map<Coordinate, Set<Coordinate>>.findBestLocationAndOrientation(): Pair<Coordinate, Orientation>? {
+    val locations = this
+    if (this.size < 12)
+        return null
+
+    val seen = mutableMapOf<Orientation, Int>()
+    val ref = locations.filter { (k, v) -> v.size == 1}.entries.first()
+    locations.filter { it != ref }
+        .flatMap { (k, v) -> v.map { k to it}}
+        .flatMap { (k, v) -> allowableOrientation(k - ref.key, v - ref.value.first()) }
+        .forEach { seen[it] = (seen[it]?:0) + 1 }
+
+    val best = seen.maxByOrNull { (k, v) -> v }?.key
+    return best?.let { orientation ->
+        val location = ref.key - orientation.rotate(ref.value.first())
+        Pair(location, orientation)
+    }
+}
+
+fun List<Scanner>.determineAllScanners(): List<Scanner> {
+    val todo = this.slice(1 until this.size).toMutableList()
+    val open = mutableListOf<Scanner>(this[0])
+    val determinedScanners = mutableListOf<Scanner>(this[0])
+    while (open.isNotEmpty()) {
+        val knownScanner = open.removeFirst()
+        val freshScanners: List<Scanner> =
+            todo
+                .mapNotNull { todoScanner ->
+                    val commonDistanceBeacons = knownScanner
+                        .toDistances()
+                        .map { BeaconDistance(it.beacons.map { knownScanner.relToAbs(it) }.toSet(), it.distance) }
+                        .intersection(todoScanner.toDistances())
+                    commonDistanceBeacons
+                        .overlappingBeaconsInDistList()
+                        .findBestLocationAndOrientation()?.let { (loc, rot) ->
+                            todoScanner.location = loc
+                            todoScanner.orientation = rot
+                            todoScanner
+                        }
+                }
+        freshScanners.forEach { scanner ->
+            open.add(scanner)
+            determinedScanners.add(scanner)
+            todo.remove(scanner)
+        }
+    }
+
+    return determinedScanners
+}
+
+
+// returns all beacons in absolute coordinates
+fun List<Scanner>.findAllBeacons(): Set<Coordinate> {
+    val beacons = this
+        .determineAllScanners()
+        .flatMap { scanner -> scanner.beaconLocations() }
+        .toSet()
+
+    return beacons
+}
+
+fun List<Scanner>.findLargestDifference():Int {
+    val scanners = this.determineAllScanners()
+
+    return (0 until scanners.size-1).flatMap{ i1 ->
+        (i1+1 until scanners.size).map { i2 ->
+            scanners[i1].location?.manhattan(scanners[i2].location!!)?:0
+        }
+    }.maxOrNull()?:0
+
+}
 
 fun main() {
     val challenge = Day19()
-    val solutions = mapOf (
+    val solutions = mapOf(
         "part1" to challenge.part1(),
         "part2" to challenge.part2(),
     )
--- src/test/kotlin/aoc2021/Day19Test.kt
+++ src/test/kotlin/aoc2021/Day19Test.kt
@@ -1,10 +1,13 @@
 package aoc2021
+import io.ktor.utils.io.*
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import java.lang.System.lineSeparator
 
 class Day19Test {
     private val input = Day19Test::class.java.getResource("/aoc2021/day19/sample.txt").readText()
-    
+    val scanners = input.toScanners()
+
     private var challenge = Day19() 
     
     @BeforeEach
@@ -15,20 +18,19 @@ class Day19Test {
     @Test
     fun testPart1() {
         val actual = challenge.part1()
-        val expected = null
+        val expected = "79"
         assertEquals(expected, actual)
     }
    
     @Test
     fun testPart2() {
         val actual = challenge.part2()
-        val expected = null
+        val expected = "3621"
         assertEquals(expected, actual)
     }
 
     @Test
     fun testParser() {
-        val scanners = input.toScanners()
         assertEquals(5, scanners.size)
         assertEquals(Coordinate(0,0,0), scanners[0].location)
         assertEquals(Orientation(Rotation.Rot0, Rotation.Rot0, Rotation.Rot0), scanners[0].orientation)
@@ -36,6 +38,130 @@ class Day19Test {
 
     @Test
     fun testIntersection() {
-        val scanners = inpu
+        val dist1 = scanners[0].toDistances()
+        val dist2 = scanners[1].toDistances()
+        val actual = dist1.intersection(dist2)
+
+        // 12 beacons --> 11 + 10 + .. + 1 pairs
+        assertEquals(11*(11+1)/2, actual.size)
+    }
+
+    @Test
+    fun testOverlappingBeacons1() {
+        val scanner1Beacons = """
+            -618,-824,-621
+            -537,-823,-458
+            -447,-329,318
+            404,-588,-901
+            544,-627,-890
+            528,-643,409
+            -661,-816,-575
+            390,-675,-793
+            423,-701,434
+            -345,-311,381
+            459,-707,401
+            -485,-357,347
+            """.trimIndent().split("\n")
+            .map { it.toCoordinate() }
+        val scanner2Beacons = """
+            686,422,578
+            605,423,415
+            515,917,-361
+            -336,658,858
+            -476,619,847
+            -460,603,-452
+            729,430,532
+            -322,571,750
+            -355,545,-477
+            413,935,-424
+            -391,539,-444
+            553,889,-390
+            """.trimIndent().split("\n")
+            .map { it.toCoordinate() }
+
+        val expected = scanner1Beacons.zip(scanner2Beacons).toMap()
+        val dist1 = scanners[0].toDistances()
+        val dist2 = scanners[1].toDistances()
+        val actual = dist1.intersection(dist2).overlappingBeaconsInDistList()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testAllowableRotations() {
+        val delta1 = Coordinate(1, 2, 3)
+        val delta2 = Coordinate(1, 2, 3)
+
+        val actual = allowableOrientation(delta1, delta2)
+        val expected = setOf(Orientation(Rotation.Rot0, Rotation.Rot0, Rotation.Rot0))
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testFindBestLocationAndRotation1() {
+        val dist1 = scanners[0].toDistances()
+        val dist2 = scanners[1].toDistances()
+        val expected = Coordinate(68,-1246,-43)
+        val actual = dist1
+            .intersection(dist2)
+            .overlappingBeaconsInDistList()
+            .findBestLocationAndOrientation()
+        assertEquals(expected, actual!!.first)
+    }
+
+    @Test
+    fun testFindBestLocationAndRotation2() {
+        scanners[1].location = Coordinate(68, -1246, -43)
+        scanners[1].orientation = Orientation(Rotation.Rot0, Rotation.Rot180, Rotation.Rot0)
+        val dist1 = scanners[1]
+            .toDistances()
+            .map { BeaconDistance(it.beacons.map { scanners[1].relToAbs(it) }.toSet(), it.distance)}
+        val dist2 = scanners[4].toDistances()
+        val expected = Coordinate(-20,-1133,1061)
+        val overlappingBeacons = dist1
+            .intersection(dist2)
+            .overlappingBeaconsInDistList()
+        val actual = overlappingBeacons
+            .findBestLocationAndOrientation()
+
+        assertEquals(expected, actual!!.first)
+
+        val expectedBeacons = """
+            459,-707,401
+            -739,-1745,668
+            -485,-357,347
+            432,-2009,850
+            528,-643,409
+            423,-701,434
+            -345,-311,381
+            408,-1815,803
+            534,-1912,768
+            -687,-1600,576
+            -447,-329,318
+            -635,-1737,486 
+            """.trimIndent()
+            .split("\n")
+            .map { it.toCoordinate() }
+            .toSet()
+
+        val scanner = scanners[4]
+        scanner.location = actual.first
+        scanner.orientation = actual.second
+        val actualBeacons = overlappingBeacons.flatMap { (k, v) -> v.map{scanner.relToAbs(it)} }.toSet()
+
+        assertEquals(expectedBeacons, actualBeacons)
+    }
+
+    @Test
+    fun testFindBeacons() {
+        val expected = Day19Test::class.java.getResource("/aoc2021/day19/beacons.txt")
+            .readText()
+            .split(lineSeparator())
+            .map { it.toCoordinate() }
+            .toSet()
+        val actual = scanners.findAllBeacons()
+
+        val difference = actual - expected
+
+        assertEquals(expected, actual)
     }
 }
