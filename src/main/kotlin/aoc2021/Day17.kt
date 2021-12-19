b8dabf7148d601b113a4b9f9ee7bc2ef1de93cf5
Peter Tillemans <pti@melexis.com>
2021-12-19 12:46:33 +0100

    day17 completed

--- src/main/kotlin/aoc2021/Day17.kt
+++ src/main/kotlin/aoc2021/Day17.kt
@@ -2,6 +2,12 @@ package aoc2021
 
 import com.google.gson.Gson
 import common.Point
+import common.minus
+import java.lang.System.`in`
+import java.lang.System.lineSeparator
+import javax.swing.text.html.HTML.Tag.P
+import kotlin.math.sqrt
+
 
 class Day17 {
     
@@ -13,23 +19,78 @@ class Day17 {
     }
     
 
-    fun part1(): String? {
-        return null
-    }
-   
-    fun part2(): String? {
-        return null
-    }
+    fun part1(): String? =
+        input.parseTargetArea().maxHeight().toString()
+
+    fun part2(): String? =
+        input.parseTargetArea().goodSpeeds().count().toString()
     
 }
 
 fun String.parseTargetArea(): Pair<Point, Point> {
     val parts = this
         .removePrefix("target area: ")
+        .removeSuffix(lineSeparator().toString())
         .split(", ")
         .flatMap { it.split("=")[1].split("..")}
+        .map { it.trim() }
         .map { it.toInt() }
     return Pair(Point(parts[0], parts[2]), Point(parts[1], parts[3]))
+}
+
+fun Pair<Point, Point>.maxHeight(): Int {
+    val yMaxDepth = this.first.second
+    val Vy0 = -yMaxDepth - 1
+    return Vy0*(Vy0 + 1)/2
+}
+
+fun Pair<Point, Point>.goodSpeeds(): List<Pair<Int, Int>>{
+    val xMax = this.second.first
+    val yMin = this.first.second
+
+    val vXmax = xMax
+    val vYmax = -yMin - 1
+    val vXmin = (-1 + sqrt(1 - 8*this.first.first.toDouble())).toInt()
+    val vYmin = yMin
+
+    val goodSpeeds = mutableSetOf<Pair<Int, Int>>()
+
+    return (vXmin..vXmax).flatMap { vX ->
+        (vYmin .. vYmax).map { vY ->
+            Pair(vX, vY)
+        }
+    }
+        .filter{ this.hitsTarget(it) }
+        .sorted()
+
+}
+
+fun  List<Pair<Int, Int>>.sorted(): List<Pair<Int, Int>> =
+    this.sortedBy { p -> p.first*1000 + p.second }
+
+
+fun Pair<Point, Point>.hitsTarget(initialSpeed: Pair<Int, Int>): Boolean {
+    val xMax = this.second.first
+    val yMin = this.first.second
+    val xMin = this.first.first
+    val yMax = this.second.second
+
+    var speed = initialSpeed
+    var pos = Point(0, 0)
+
+    while (pos.first <= xMax && pos.second >= yMin) {
+        pos = pos + speed
+        if (speed.first > 0) {
+            speed -= Pair(1, 1)
+        } else {
+            speed -= Pair(0, 1)
+        }
+        if (pos.first in xMin..xMax && pos.second in yMin..yMax) {
+            return true
+        }
+
+    }
+    return false
 }
 
 fun main() {
--- src/test/kotlin/aoc2021/Day17Test.kt
+++ src/test/kotlin/aoc2021/Day17Test.kt
@@ -2,11 +2,22 @@ package aoc2021
 import common.Point
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import java.lang.System.lineSeparator
 
 class Day17Test {
-    private val input = """target area: x=20..30, y=-10..-5"""
-    
-    private var challenge = Day17() 
+    private val input = """target area: x=20..30, y=-10..-5${lineSeparator()}""".trimMargin()
+
+    private val samples = Day17Test::class.java.getResource("/aoc2021/day17/sample.txt")!!.readText()
+        .split(lineSeparator())
+        .flatMap { it.split(" +".toRegex()) }
+        .flatMap {line -> line.split(",")
+            .filter {it.isNotEmpty()}
+            .map{it.toInt()}
+            .zipWithNext()
+        }
+        .sorted()
+
+    private var challenge = Day17()
     
     @BeforeEach
     fun setUp() {
@@ -19,18 +30,30 @@ class Day17Test {
         val expected = Pair(Point(20, -10), Point(30, -5))
         assertEquals(expected, targetArea)
     }
-   
+
+    @Test
+    fun testMaxHeight() {
+        val actual = input.parseTargetArea().maxHeight()
+        assertEquals(45, actual)
+    }
+
+    @Test
+    fun testGoodSpeeds() {
+        val actual = input.parseTargetArea().goodSpeeds()
+        val expected = samples.sorted()
+        assertEquals(expected, actual)
+    }
     @Test
     fun testPart1() {
         val actual = challenge.part1()
-        val expected = null
+        val expected = "45"
         assertEquals(expected, actual)
     }
    
     @Test
     fun testPart2() {
         val actual = challenge.part2()
-        val expected = null
+        val expected = "112"
         assertEquals(expected, actual)
     }
     
