42b82ad68e7bf3a047771105a3e4937d0f31733b
ptillemans <pti@snamellit.com>
2021-12-23 12:53:39 +0100

    day 20

--- src/main/kotlin/aoc2021/Day20.kt
+++ src/main/kotlin/aoc2021/Day20.kt
@@ -1,30 +1,116 @@
 package aoc2021
 
-import com.google.gson.Gson 
+import com.google.gson.Gson
+import common.Point
+import java.lang.System.lineSeparator
+
+typealias ImageEnhancementAlgorithm = Array<Boolean>
+typealias Bounds = Pair<Point, Point>
+data class Day20Image(val bounds: Bounds, val image: Set<Point>) {
+    fun inBounds(pos: Point) =
+        (pos.first in bounds.first.first..bounds.second.first)
+                && (pos.second in bounds.first.second .. bounds.second.second)
+}
 
 class Day20 {
-    
+
     var input: String
-    
+
+
     init {
         val filename = "/aoc2021/day20/input.txt"
         input = Day20::class.java.getResource(filename).readText()
     }
-    
 
-    fun part1(): String? {
-        return null
+
+    fun part1(): String =
+        input.parseDay20().let { (eia, img) ->
+            val image1 = img.enhance(eia, 0)
+            val image2 = image1.enhance(eia, if(eia[0]) 1 else 0)
+            println("${image1.image.size} - ${image2.image.size}")
+            image2.image.count().toString()
+        }
+
+    fun part2(): String {
+        val (eia, img) = input.parseDay20()
+        var image = img
+            (0 until 50).forEach {
+                val default = if (eia[0]) it%2 else 0
+                image = image.enhance(eia, default)
+            }
+        return image.image.count().toString()
     }
-   
-    fun part2(): String? {
-        return null
-    }
-    
+
+
+
 }
+
+fun String.parseDay20(): Pair<ImageEnhancementAlgorithm, Day20Image> {
+    val parts = this.split(lineSeparator() + lineSeparator())
+    val eia = parts[0].map { c -> (c == '#') }
+        .toTypedArray()
+    val image = parts[1].parseImage()
+    return Pair(eia, image)
+}
+
+fun String.parseImage(offset: Point = Point(0, 0)): Day20Image {
+    val img = this
+        .split(lineSeparator())
+        .filter { isNotBlank() }
+        .flatMapIndexed { row, line ->
+            line
+                .mapIndexed { col, ch ->
+                    if (ch == '#') offset + Point(row, col) else null
+                }
+                .filterNotNull()
+        }
+        .toSet()
+    val bounds = img.bounds()
+    return Day20Image(bounds, img)
+}
+
+fun Day20Image.enhance(iae: ImageEnhancementAlgorithm, default: Int = 0): Day20Image =
+    this.bounds.let { (min, max) ->
+
+        val image = (min.first - 1..max.first + 1).flatMap { r ->
+                (min.second - 3..max.second + 3).map { c ->
+                Point(r, c)
+            }
+        }
+            .filter { pos ->
+                val pc = this.pixelCode(pos, default)
+                iae[pc]
+            }
+            .toSet()
+        val bounds = image.bounds()
+        Day20Image(bounds, image)
+    }
+
+fun Set<Point>.bounds(): Pair<Point, Point> {
+    val minR = this.minOf { it.first }
+    val maxR = this.maxOf { it.first }
+
+    val minC = this.minOf { it.second }
+    val maxC = this.maxOf { it.second }
+
+    return Pair(Point(minR, minC), Point(maxR, maxC))
+}
+
+fun Day20Image.pixelCode(pos: Point, default: Int) =
+    (-1..1).flatMap { dr ->
+        (-1..1).map { dc ->
+            val p = pos + Point(dr, dc)
+            if (this.inBounds(p))
+                if (this.image.contains(pos + Point(dr, dc))) 1 else 0
+            else
+                default
+        }
+    }
+        .reduce { acc, x -> acc * 2 + x }
 
 fun main() {
     val challenge = Day20()
-    val solutions = mapOf (
+    val solutions = mapOf(
         "part1" to challenge.part1(),
         "part2" to challenge.part2(),
     )
--- src/test/kotlin/aoc2021/Day20Test.kt
+++ src/test/kotlin/aoc2021/Day20Test.kt
@@ -1,12 +1,15 @@
 package aoc2021
+import common.Point
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import java.lang.System.lineSeparator
 
 class Day20Test {
-    private val input = """
-""".trimIndent()
-    
-    private var challenge = Day20() 
+    private val input = Day20Test::class.java.getResource("/aoc2021/day20/sample.txt").readText()
+    private val enhance1 = Day20Test::class.java.getResource("/aoc2021/day20/enhance1.txt").readText()
+    private val enhance2 = Day20Test::class.java.getResource("/aoc2021/day20/enhance2.txt").readText()
+
+    private var challenge = Day20()
     
     @BeforeEach
     fun setUp() {
@@ -16,15 +19,47 @@ class Day20Test {
     @Test
     fun testPart1() {
         val actual = challenge.part1()
-        val expected = null
+        val expected = "35"
         assertEquals(expected, actual)
     }
    
     @Test
     fun testPart2() {
         val actual = challenge.part2()
-        val expected = null
+        val expected = "3351"
         assertEquals(expected, actual)
     }
-    
+
+
+    @Test
+    fun testParseInput() {
+        val (eia, img) = input.parseDay20()
+        val expectedEia = input.split(lineSeparator()).first().count { it == '#' }
+        val expectedImage = input.split(lineSeparator()).drop(2).joinToString("").count { it == '#' }
+        assertEquals(expectedEia, eia.count { it })
+        assertEquals(expectedImage, img.image.size)
+    }
+
+    @Test
+    fun testEnhanceImage() {
+        val (iea, img) = input.parseDay20()
+        val expected  = enhance1.parseImage(offset = Point(-1, -1))
+        val enhancedImage = img.enhance(iea)
+        assertEquals(expected, enhancedImage)
+    }
+
+    @Test
+    fun testEnhance2Image() {
+        val (iea, img) = input.parseDay20()
+        val expected  = enhance2.parseImage(offset = Point(-2, -2))
+        val enhancedImage = img.enhance(iea).enhance(iea)
+        assertEquals(expected, enhancedImage)
+    }
+
+    @Test
+    fun testPixelCode() {
+        val (iea, img) = input.parseDay20()
+        val expected = 34
+        val actual = img.pixelCode(Point(1,1), 0)
+    }
 }
